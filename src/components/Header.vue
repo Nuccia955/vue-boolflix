fbd5c09ea2d892e8fd049080cb33ba96d254f0c8
Nuccia955 <elenamunaro1995@gmail.com>
2021-11-29 11:58:10 +0100

    call API

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,47 @@
 <template>
     <header>
-        <input type="text" placeholder="Search movie">
-        <button>Search</button>
+        <input 
+            type="text" 
+            placeholder="Search movie"
+            v-model="searchInput"
+        >
+        <button 
+            @click="getMovieList"
+        >
+            Search
+        </button>
     </header>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
     name: 'Header',
+    data() {
+        return {
+            APIurl: 'https://api.themoviedb.org/3/search/movie',
+            searchInput: '',
+        }
+    },
+    methods: {
+        getMovieList() {
+            axios.get(this.APIurl, {
+                params: {
+                    api_key: '9523b234fd1c8550cfc9dea66c01f6f2',
+                    query: 'Fantozzi',
+                }
+            })
+            .then(response => {
+                // handle success
+                console.log(response.data.results);
+                this.$emit('genMovieList', response.data.results)
+            })
+            .catch(error => {
+                // handle error
+                console.log(error);
+            })
+        },
+    }
 }
 </script>
 
